652114fc56b1c74a91f99bb48373d220e09bc7d2
liuhonghi <23306244@qq.com>
2023-06-14 23:07:08 +0800

    Fisrt Commit

--- test/test/main.c
+++ test/test/main.c
@@ -25,7 +25,9 @@ void game()
     DisplayBoard(show, ROW, COL);
     
     SetMine(mine,ROW,COL);
-    DisplayBoard(mine, ROW, COL);
+    //DisplayBoard(mine, ROW, COL);
+    
+    FindMine(mine,show, ROW, COL);
 }
 
 void menu()
